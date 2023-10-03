85eb6519495775c88c54f20e8b535c1184d5c2b1
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-04 06:48:46 +0900

    첫번째 점으로 이동하는 기능 구현 완료

--- drill06.py
+++ drill06.py
@@ -30,12 +30,12 @@ def reset_world():
     action = 3
 
     points = [(100, 900), (1200, 800), (500, 100)]
-    # set_new_target_arrow()
+    set_new_target_arrow()
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t, action, frame
     sx, sy = cx, cy # p1: 시작점
-    hx, hy = randint(0, TUK_WIDTH), randint(0,TUK_HEIGHT) # p2: 끝점
+    hx, hy = points[0] # p2: 끝점
     t = 0.0
     action = 1 if sx < hx else 0
     frame = 0
@@ -52,13 +52,13 @@ def render_world():
 def update_world():
     global frame, cx, cy, t
     frame = (frame + 1) % 8
-    # if t <= 1.0:
-    #     cx = (1-t) * sx + t * hx # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
-    #     cy = (1-t) * sy + t * hy
-    #     t += 0.001
-    # else:
-    #     cx, cy = hx, hy
-    #     set_new_target_arrow()
+    if t <= 1.0:
+        cx = (1-t) * sx + t * hx # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
+        cy = (1-t) * sy + t * hy
+        t += 0.001
+    else:
+        cx, cy = hx, hy
+        set_new_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
